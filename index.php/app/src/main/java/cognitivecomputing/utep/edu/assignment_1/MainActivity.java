109217da7bcd1ef71fb16115ffc3b72be6a0ce14
Oscar Ricaud <oiricaud@miners.utep.edu>
2016-09-08 12:08:36 -0600

    Got watson to speak to me.

---
+++ index.php/app/src/main/java/cognitivecomputing/utep/edu/assignment_1/MainActivity.java
@@ -0,0 +1,39 @@
+package cognitivecomputing.utep.edu.assignment_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addButtonListener();
+    }
+
+    private void addButtonListener()
+    {
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputText = getTextFromEditText();
+                Toast.makeText(getApplicationContext(), "Hi! Your input: " + inputText, Toast.LENGTH_SHORT).show();
+
+                TextToSpeechWebServiceHandler ttswsh = new TextToSpeechWebServiceHandler(getApplicationContext());
+                ttswsh.execute(inputText);
+            }
+        });
+    }
+    private String getTextFromEditText()
+    {
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        return editText.getText().toString();
+    }
+}
